13ecde7021a660f1b20317e7cb3abb88cdf59b88
Mruddin2000 <mohammeduddin1420@gmail.com>
2022-09-09 00:12:36 +0100

    v1

---
+++ JavaScripts.js
@@ -0,0 +1,310 @@
+function validateForm(){
+    validatesEmail();
+
+ 
+
+}
+
+    function validatesEmail(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform.email.focus();
+        myform.email.style.background="red";
+        }
+         else  if (result === true){
+        myform.email.style.background="white";
+        window.location="OrderConfirmationOfBritishMuseum.html";
+
+        }
+}
+
+function Like(){
+        
+        alert ("You have liked this Review");      
+    }
+    function Dislike(){
+        
+        alert ("You have Disliked this Review");      
+    }
+
+    function Fav(){
+        
+        alert ("You have Added this to your Favourite Page");      
+    }
+
+function validateForm2(){
+    validatesEmail2();
+
+ 
+
+}
+
+
+    function validatesEmail2(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform2.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform2.email.focus();
+        myform2.email.style.background="red";
+        }
+         else  if (result === true){
+        myform2.email.style.background="white";
+        window.location="OrderConfirmationOfBuckinghamPalace.html";
+
+        }
+}
+
+function validateForm3(){
+    validatesEmail3();
+
+ 
+
+}
+
+    function validatesEmail3(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform3.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform3.email.focus();
+        myform3.email.style.background="red";
+        }
+         else  if (result === true){
+        myform3.email.style.background="white";
+        window.location="OrderConfirmationOfLondonEye.html";
+
+        }
+}
+
+ function validateForm4(){
+    validatesEmail4();
+
+ 
+
+}
+
+    function validatesEmail4(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform4.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform4.email.focus();
+        myform4.email.style.background="red";
+        }
+         else  if (result === true){
+        myform4.email.style.background="white";
+        window.location="OrderConfirmationOfVictoria&AlbertMuseum.html";
+
+        }
+}
+
+function validateForm5(){
+    validatesEmail5();
+
+ 
+
+}
+
+    function validatesEmail5(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform5.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform5.email.focus();
+        myform5.email.style.background="red";
+        }
+         else  if (result === true){
+        myform5.email.style.background="white";
+        window.location="OrderConfirmationOfStonehenge.html";
+
+        }
+}
+
+function validateForm6(){
+    validatesEmail6();
+
+ 
+
+}
+
+    function validatesEmail6(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform6.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform6.email.focus();
+        myform6.email.style.background="red";
+        }
+         else  if (result === true){
+        myform6.email.style.background="white";
+        window.location="OrderConfirmationOfTowerOfLondon.html";
+
+        }
+}
+
+
+function validateForm7(){
+    validatesEmail7();
+
+ 
+
+}
+
+    function validatesEmail7(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform7.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform7.email.focus();
+        myform7.email.style.background="red";
+        }
+         else  if (result === true){
+        myform7.email.style.background="white";
+        window.location="OrderConfirmationOfRegentPark.html";
+
+        }
+}
+
+
+function validateForm8(){
+    validatesEmail8();
+
+ 
+
+}
+
+    function validatesEmail8(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform8.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform8.email.focus();
+        myform8.email.style.background="red";
+        }
+         else  if (result === true){
+        myform8.email.style.background="white";
+        window.location="OrderConfirmationOfHydePark.html";
+
+        }
+}
+
+
+function validateForm9(){
+    validatesEmail9();
+
+ 
+
+}
+
+    function validatesEmail9(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform9.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform9.email.focus();
+        myform9.email.style.background="red";
+        }
+         else  if (result === true){
+        myform9.email.style.background="white";
+        window.location="OrderConfirmationOfStJamesPark.html";
+
+        }
+}
+
+
+function validateForm10(){
+    validatesEmail10();
+
+ 
+
+}
+
+    function validatesEmail10(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform10.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform10.email.focus();
+        myform10.email.style.background="red";
+        }
+         else  if (result === true){
+        myform10.email.style.background="white";
+        window.location="OrderConfirmationOfClassicBusTour.html";
+
+        }
+}
+
+
+
+function validateForm11(){
+    validatesEmail11();
+
+ 
+
+}
+
+    function validatesEmail11(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform11.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform11.email.focus();
+        myform11.email.style.background="red";
+        }
+         else  if (result === true){
+        myform11.email.style.background="white";
+        window.location="OrderConfirmationOfCityCruises.html";
+
+        }
+}
+
+
+
+function validateForm12(){
+    validatesEmail12();
+
+ 
+
+}
+
+    function validatesEmail12(){
+    var pattern = new RegExp("@");
+    var result = pattern.test(myform12.email.value);
+
+
+    if (result === false){
+        alert("You have not entered a valid email");
+        myform12.email.focus();
+        myform12.email.style.background="red";
+        }
+         else  if (result === true){
+        myform12.email.style.background="white";
+        window.location="OrderConfirmationOfWarnerBrosStudioTourLondon.html";
+
+        }
+}
